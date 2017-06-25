c5913c1a91f10e6383945bc191f770fe972ba2e1
Arkadiusz Halicki <halicki.arkadiusz@gmail.com>
2017-06-25 12:08:25 +0200

    Add default fish for virtualfish

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,1 +1,4 @@
-eval (python3 -m virtualfish auto_activation)
+set -x PATH $PATH /usr/local/sbin
+set -x PATH $PATH ~/.bin
+set -x VIRTUALFISH_DEFAULT_PYTHON python3.6
+eval (python -m virtualfish auto_activation)
