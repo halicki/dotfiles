df3761e4051cb8bae903178062d0c1c1dd3617b2
Arkadiusz Halicki <a.halicki@partner.samsung.com>
2017-07-03 10:08:04 +0200

    remove setup of pyenv

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,10 +1,2 @@
 set -x PATH $PATH /usr/local/sbin
 set -x PATH $PATH ~/.bin
-
-# Load pyenv automatically by appending
-# the following to ~/.config/fish/config.fish:
-status --is-interactive; and source (pyenv init -|psub)
-
-# Load pyenv-virtualenv automatically by adding
-# the following to ~/.config/fish/config.fish:
-status --is-interactive; and source (pyenv virtualenv-init -|psub)
